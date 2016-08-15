b4f4c4b45dbd941a9ee53aac6ba04b7d7d332eba
LeoVS09 <LeoVS09@mail.ru>
2016-08-16 01:41:30 +0300

    Complate post entity for reg and loginIn

--- src/main/java/hello/PostController.java
+++ src/main/java/hello/PostController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,9 +34,25 @@ public class PostController {
     }
 
     @RequestMapping(value="/user/registration", method = RequestMethod.POST)
-    public User Registration(@RequestBody User user){
+    public ResponseEntity<Void> Registration(@RequestHeader HttpHeaders headers, @RequestBody User user){
         log.info("------->" + user.getLogin() + ": " + user.getPassword());
-        user.setPassword("OK");
-        return user;
+        if(user.getLogin() == "Trol" && user.getPassword() == "lol") return new ResponseEntity<Void>(HttpStatus.OK);
+        return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
     }
+
+    @RequestMapping(value="/user/login", method = RequestMethod.POST)
+    public ResponseEntity<String> LoginIn(@RequestHeader HttpHeaders headers, @RequestBody User user){
+        String token = "lololol";
+        log.info("------->" + user.getLogin() + ": " + user.getPassword());
+        if(user.getLogin() == "Trol" && user.getPassword() == "lol") return new ResponseEntity<String>(token, HttpStatus.OK);
+        return new ResponseEntity<String>("", HttpStatus.BAD_REQUEST);
+    }
+
+//    @RequestMapping(value="/game/create", method = RequestMethod.POST)
+//    public ResponseEntity<String> gameCreate(@RequestHeader HttpHeaders headers, @RequestBody Game game){
+//        if(headers.get("Authorization").equals("lololol")) return new ResponseEntity<String>("", HttpStatus.BAD_REQUEST);
+//        long id = 101;
+//        if(game.getName() == "Trol" && game.getPassword() == "lol") return new ResponseEntity<String>(Long.toString(id), HttpStatus.OK);
+//        return new ResponseEntity<String>("", HttpStatus.BAD_REQUEST);
+//    }
 }
--- src/main/java/hello/User.java
+++ src/main/java/hello/User.java
@@ -7,6 +7,7 @@ public class User {
     private String login;
     private String password;
 
+    public User(){}
     public User(String login, String password) {
         this.login = login;
         this.password = password;
